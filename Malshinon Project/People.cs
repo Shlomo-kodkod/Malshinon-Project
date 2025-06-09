099342a12571e94e3fc853f3c236bfc815b6dccc
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-06-09 15:07:27 +0300

    Update People.cs

--- Malshinon Project/People.cs
+++ Malshinon Project/People.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,34 +8,23 @@ namespace Malshinon_Project
 {
     internal class People
     {
-        static internal string[] types = new string[] { "reporter", "target", "both", "potential_agent" };
         internal int id;
         internal string firstName;
         internal string lastName;
         internal string secretCode;
         internal string type;
-        internal int numReports = 0;
-        internal int numMentions = 0;
+        internal int numReports;
+        internal int numMentions;
 
-        internal People(string FirstName, string LastName, string SecretCode)
+        internal People(int Id, string FirstName, string LastName, string SecretCode, string NewType, int NumReports, int NumMentions)
         {
-            Guid g = Guid.NewGuid();
+            this.id = Id;
             this.firstName = FirstName;
             this.lastName = LastName;
-            this.secretCode = Convert.ToBase64String(g.ToByteArray());
-            this.type = types[0];
-
-        }
-
-
-        internal People(string FirstName, string LastName, string SecretCode, string NewType)
-        {
-            Guid g = Guid.NewGuid();
-            this.firstName = FirstName;
-            this.lastName = LastName;
-            this.secretCode = Convert.ToBase64String(g.ToByteArray());
+            this.secretCode = SecretCode;
             this.type = NewType;
-
+            this.numReports = NumReports;
+            this.numMentions = NumMentions;
         }
 
     }
