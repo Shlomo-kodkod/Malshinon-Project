330a68674406c5a71d9fc7c926469bcdd9996843
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-10 09:35:46 +0300

    add intel report class

---
+++ Malshinon Project/IntelReport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon_Project
+{
+    internal class IntelReport
+    {
+        int id;
+        int reporterId;
+        int targetId;
+        string text;
+        DateTime timestemp;
+
+        internal IntelReport(int Id, int ReporterId, int TargetId, string Text, DateTime TimeStemp)
+        {
+            this.id = Id;
+            this.reporterId = ReporterId;
+            this.targetId = TargetId;
+            this.text = Text;
+            this.timestemp = TimeStemp;
+        }
+
+        internal IntelReport() { }
+    }
+}
