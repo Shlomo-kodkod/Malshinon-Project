531e07dfdfb05bad8a8898a49f7d567cdf422eb6
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-11 12:39:22 +0300

    add alerts class

---
+++ Malshinon Project/Alerts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon_Project
+{
+    internal class Alerts
+    {
+        int id;
+        int targetId;
+        DateTime creatAt;
+        string reason;
+
+        public Alerts(int Id, int TargetId, DateTime CreatAt, string Reason)
+        {
+            this.id = Id;
+            this.targetId = TargetId;
+            this.creatAt = CreatAt;
+            this.reason = Reason;
+        }
+    }
+}
